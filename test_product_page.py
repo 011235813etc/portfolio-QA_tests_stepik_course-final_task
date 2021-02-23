1857fd2be4e810aa6232dd55fcf1152a5e5ad6a9
Ilgiz <ii01123581321@gmail.com>
2021-02-24 00:30:45 +0500

    Prepare for code review

--- test_product_page.py
+++ test_product_page.py
@@ -30,6 +30,7 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
         # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
@@ -57,6 +58,7 @@ class TestUserAddToBasketFromProductPage():
         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
     ])
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser, link):
     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page = ProductPage(browser, link)
@@ -100,12 +102,15 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
+
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
